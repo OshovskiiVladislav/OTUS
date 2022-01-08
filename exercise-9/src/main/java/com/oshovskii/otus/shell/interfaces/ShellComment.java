94e9de786fe47f31870304fec558060662573ae4
Qwerty <rosna-m@mail.ru>
2022-01-08 15:37:06 +0800

    Commit 8. add shell

---
+++ exercise-9/src/main/java/com/oshovskii/otus/shell/interfaces/ShellComment.java
@@ -0,0 +1,12 @@
+package com.oshovskii.otus.shell.interfaces;
+
+public interface ShellComment {
+    Long publishCountComments();
+    String publishCommentByID(Long commentId);
+
+    String publishAllComment();
+    String publishCommentByText(String text);
+
+    String saveComment(String text);
+    String deleteCommentByID(Long commentId);
+}
