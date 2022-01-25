0f646d3e4229bcea306dcc3e410f8c28f2388098
Qwerty <rosna-m@mail.ru>
2022-01-26 00:03:51 +0800

    Commit 3. add configurations

---
+++ exercise-13/src/main/java/com/oshovskii/otus/MainApplication.java
@@ -0,0 +1,13 @@
+package com.oshovskii.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
