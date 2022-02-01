6dfc99b3ec416f4f977d18fa13e90ec0578a10a9
Qwerty <rosna-m@mail.ru>
2022-02-01 16:51:32 +0800

    Commit 12. FRONTEND add project

---
+++ exercise-17/frontend/libraryapp/src/app/book.service.ts
@@ -0,0 +1,29 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+import { environment } from 'src/environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class BookService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiServerUrl}/api/v1/books`);
+  }
+
+  public addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(`${this.apiServerUrl}/api/v1/books`, book);
+  }
+
+  public updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiServerUrl}/api/v1/books`, book);
+  }
+
+  public deleteBook(bookId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/v1/books/${bookId}`);
+  }
+}
