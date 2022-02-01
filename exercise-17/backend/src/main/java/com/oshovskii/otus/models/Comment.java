9bf13674e7030db1f1dae3eab8345a20bdaf4508
Qwerty <rosna-m@mail.ru>
2022-02-01 16:41:01 +0800

    Commit 2. BACKEND add entities

---
+++ exercise-17/backend/src/main/java/com/oshovskii/otus/models/Comment.java
@@ -0,0 +1,25 @@
+package com.oshovskii.otus.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "text")
+    @Lob
+    private String text;
+}
