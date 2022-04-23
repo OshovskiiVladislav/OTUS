cea4c91a7ca81d6d4ca1e106fd7c595db688b313
Qwerty <rosna-m@mail.ru>
2022-04-23 14:58:44 +0800

    Commit 8. add WorkService

--- exercise-29-spring-integration/src/main/java/com/oshovskii/otus/config/IntegrationConfig.java
+++ exercise-29-spring-integration/src/main/java/com/oshovskii/otus/config/IntegrationConfig.java
@@ -1,7 +1,7 @@
 package com.oshovskii.otus.config;
 
 
-import com.oshovskii.otus.service.WorkService;
+import com.oshovskii.otus.services.WorkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
